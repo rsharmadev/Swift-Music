0f0a7da24719a57b459f33f28fa44af30b8877d9
NickFet <73346471+NickFet@users.noreply.github.com>
2021-06-13 11:46:37 -0400

    nevermind

--- postcss.config.js
+++ postcss.config.js
@@ -2,12 +2,5 @@ module.exports = {
     plugins: [
         require('tailwindcss'),
         require('autoprefixer'),
-        require('@fullhuman/postcss-purgecss')({
-            content: [
-                './public/build/index.html',
-
-            ],
-            defaultExtractor: content => content.match(/[A-Za-z0-9_:/]+/g) || []
-        })
     ]
 }
